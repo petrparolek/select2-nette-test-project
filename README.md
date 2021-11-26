75e03382796c17a47d268977f88fc2225d542e5d
Petr Parolek <petr.parolek@gmail.com>
2021-11-26 23:11:09 +0100

    initial commit with bug

---
+++ README.md
@@ -0,0 +1,16 @@
+Select2 Nette test project
+==========================
+
+# Install using [docker](https://github.com/docker/docker/)
+
+1) At first, use GIT to download this project.
+
+2) Run command:
+
+   ```
+   docker-compose up
+   ```
+
+3) Open http://localhost
+
+If you need enter into container (such as `composer update`) run command `docker-compose run web bash`.
---
+++ app/Presenters/HomepagePresenter.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types = 1);
+
+namespace App\Presenters;
+
+use Contributte\Forms\IApplicationFormFactory;
+use Contributte\Forms\Rendering\Bootstrap4VerticalRenderer;
+use Nette\Application\UI\Form;
+
+final class HomepagePresenter extends BasePresenter
+{
+
+	/** @var IApplicationFormFactory */
+	private $formFactory;
+
+	public function __construct(IApplicationFormFactory $formFactory)
+	{
+		parent::__construct();
+		$this->formFactory = $formFactory;
+	}
+
+	protected function createComponentTestForm(): Form
+	{
+		$form = $this->formFactory->create();
+		$form->setRenderer(new Bootstrap4VerticalRenderer());
+
+		$form->addText('name', 'Name:')
+			->setRequired();
+
+		$form->addPassword('password', 'Password:')
+			->setRequired()
+			->addRule(Form::MIN_LENGTH, 'Password must bea at least %d characters long', 6);
+
+		$form->addPassword('passwordVerify', 'Password again:')
+			->setRequired('Please set the password again for check')
+			->addRule(Form::EQUAL, 'Passwords are not equal', $form['password']);
+
+		$roles = [
+			'admin' => 'admin',
+			'moderator' => 'moderator',
+			'user' => 'user',
+			'guest' => 'guest',
+		];
+		$form->addSelect('role', 'Role', $roles)
+			->setHtmlAttribute('class','select2-example')
+			->setPrompt('-- Select role --')
+			->setRequired(true);
+
+		$form->addMultiSelect('roles', 'Roles', $roles)
+			->setHtmlAttribute('class','select2-example')
+			->setRequired(true);
+
+		$form->addSelect('another_role', 'Role', $roles)
+			->setPrompt('-- Select role --')
+			->setRequired(true);
+
+		$form->addMultiSelect('another_roles', 'Roles', $roles)
+			->setRequired(true);
+
+		$form->addSubmit('signup', 'Sign up');
+
+		$form->onSuccess[] = function (Form $form) {
+			dump($form->getValues());
+			bdump($form->getValues());
+		};
+
+		return $form;
+	}
+
+}
